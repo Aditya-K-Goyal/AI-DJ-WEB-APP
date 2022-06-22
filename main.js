c11b10a9d7259f314cd382fefa2aaeafe4b51781
Aditya K Goyal <101977885+Aditya-K-Goyal@users.noreply.github.com>
2022-06-22 12:29:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
+rightWristX = 0;
 function preload()
 {
     song = loadSound("music.mp3");
@@ -8,7 +12,24 @@ function setup()
     canvas = createCanvas(600,500);
     canvas.center();
     video = createCapture(VIDEO);
-    video.hide();
+    video.hide(); 
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function gotPoses(results)
+{
+    if(results.length >0 ){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left wrist X = "+ leftWristX + " , left wrist Y = " + leftWristY);
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("right wrist X = "+ rightWristX + " , right wrist Y = " + rightWristY);
+    }
+}
+function modelLoaded(){
+    console.log("PoseNet is initialized");
 }
 function draw()
 {
@@ -17,4 +38,6 @@ function draw()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
