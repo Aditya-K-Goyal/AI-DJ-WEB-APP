916855030385b9ca51cf6401c7bb5c21f045b500
Aditya K Goyal <101977885+Aditya-K-Goyal@users.noreply.github.com>
2022-06-25 09:34:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristY = 0;
 rightWristX = 0;
+scoreLeftWrist = 0;
 function preload()
 {
     song = loadSound("music.mp3");
@@ -20,12 +21,15 @@ function gotPoses(results)
 {
     if(results.length >0 ){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("left wrist X = "+ leftWristX + " , left wrist Y = " + leftWristY);
         rightWristY = results[0].pose.rightWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
         console.log("right wrist X = "+ rightWristX + " , right wrist Y = " + rightWristY);
+
     }
 }
 function modelLoaded(){
@@ -34,6 +38,17 @@ function modelLoaded(){
 function draw()
 {
     image(video,0,0,600,500);
+    fill("red");
+    stroke("red");
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        InNumberLeftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberLeftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    
+    }
 }
 function play()
 {
